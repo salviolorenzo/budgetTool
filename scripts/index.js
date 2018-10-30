6fcc46bde98e4716fa19414b7839e12b72e54c8a
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-10-30 16:59:37 -0400

    adds js query selectors

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,17 @@
-// 
+// Summary variables
+const budgetHeader = document.querySelector('[data-budget-header]');
+const total = document.querySelector('[data-balance]');
+const totalIncome = document.querySelector('[data-income]');
+const totalExpense = document.querySelector('[data-expense]');
+const expensePercent = document.querySelector('[data-percent]');
+
+// Form variables
+const addForm = document.querySelector('[data-form]');
+const select = document.querySelector('[data-select]');
+const description = document.querySelector('[data-description]');
+const amount = document.querySelector('[data-value]');
+const submitButton = document.querySelector('[data-submit]');
+
+// Balance sheet variables
+const incomeDiv = document.querySelector('[data-add-income]');
+const expenseDiv = document.querySelector('[data-add-expense]');
