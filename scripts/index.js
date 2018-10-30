8387675db46644ac86f1f0e9bfb887cf1ccfeafc
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-10-30 17:37:21 -0400

    adds submit event handler

--- scripts/index.js
+++ scripts/index.js
@@ -8,10 +8,47 @@ const expensePercent = document.querySelector('[data-percent]');
 // Form variables
 const addForm = document.querySelector('[data-form]');
 const select = document.querySelector('[data-select]');
-const description = document.querySelector('[data-description]');
-const amount = document.querySelector('[data-value]');
 const submitButton = document.querySelector('[data-submit]');
 
 // Balance sheet variables
 const incomeDiv = document.querySelector('[data-add-income]');
 const expenseDiv = document.querySelector('[data-add-expense]');
+
+// Get user input
+// Determine if positive or negative
+// Add to either total income or expenses
+// create div in balance sheet for item
+
+
+// ================================================================
+// GETTING USER INPUT
+// ================================================================
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let description = event.target.elements.description.value;
+    let amount = event.target.elements.amount.value;
+    let isIncome = select.value === '+';
+    if (isIncome) {
+        incomeDiv.appendChild(makeDivs(description, amount));
+    }
+    else {
+        expenseDiv.appendChild(makeDivs(description, amount));
+    }
+}
+
+addForm.addEventListener('submit', handleSubmit);
+
+function makeDivs(description, amount) {
+    let newItem = document.createElement('div');
+    let itemDesc = document.createElement('p');
+    let itemAmt = document.createElement('p');
+    itemDesc.textContent = description;
+    itemAmt.textContent = amount;
+    newItem.appendChild(itemDesc);
+    newItem.appendChild(itemAmt);
+    newItem.classList.add('added');
+    return newItem;
+}
+
