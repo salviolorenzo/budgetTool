39491d85ef291dcc643c5fe0c0d4b3fbb5a15527
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-11-21 17:09:35 -0800

    adds styles

--- scripts/index.js
+++ scripts/index.js
@@ -34,133 +34,137 @@ let tipArray = [];
 // GETTING USER INPUT
 // ================================================================
 for (item of budgetObject) {
-    budgetArray.push(item);
+  budgetArray.push(item);
 }
 for (item of calculatorObject) {
-    tipArray.push(item);
+  tipArray.push(item);
 }
 
 
 triggerElement.addEventListener('click', function () {
-    for (item of budgetArray) {
-        item.classList.toggle('hidden');
-    }
-    for (item of tipArray) {
-        item.classList.toggle('hidden');
-    }
+  for (item of budgetArray) {
+    item.classList.toggle('hidden');
+  }
+  for (item of tipArray) {
+    item.classList.toggle('hidden');
+  }
 
-    header.classList.toggle('darkMode');
-    footer.classList.toggle('darkMode');
+  header.classList.toggle('darkMode');
+  footer.classList.toggle('darkMode');
 
-    if (headerText.textContent === 'Budgetter') {
-        headerText.textContent = 'Tip Calculator';
-        triggerElement.textContent = 'Budgetter';
-    }
-    else if (headerText.textContent === 'Tip Calculator') {
-        headerText.textContent = 'Budgetter';
-        triggerElement.textContent = 'Tip Calculator';
-    }
+  if (headerText.textContent === 'Budgetter') {
+    headerText.textContent = 'Tip Calculator';
+    triggerElement.textContent = 'Budgetter';
+  }
+  else if (headerText.textContent === 'Tip Calculator') {
+    headerText.textContent = 'Budgetter';
+    triggerElement.textContent = 'Tip Calculator';
+  }
 })
 
 
 
 
 function handleSubmit(event) {
-    event.preventDefault();
-    let description = event.target.elements.description.value;
-    let amount = parseFloat(event.target.elements.amount.value);
-    let isIncome = select.value === '+';
-    if (description !== '' && amount !== '') {
-        if (isIncome) {
-            incomeDiv.appendChild(makeDivs(description, amount));
-            totalIncomeAmt += amount;
-        }
-        else {
-            expenseDiv.appendChild(makeDivs(description, amount));
-            totalExpenseAmt += amount;
-        }
-        totalIncome.textContent = totalIncomeAmt;
-        totalExpense.textContent = totalExpenseAmt;
-        expensePercent.textContent = `${drawPercent(totalIncomeAmt, totalExpenseAmt).toFixed(2)}%`;
-        total.textContent = drawTotal(totalIncomeAmt, totalExpenseAmt);
-        description.value = '';
-        amount.value = '';
+  event.preventDefault();
+  let description = event.target.elements.description.value;
+  let amount = parseFloat(event.target.elements.amount.value);
+  let isIncome = select.value === '+';
+  if (description !== '' && amount !== '') {
+    if (isIncome) {
+      incomeDiv.appendChild(makeDivs(description, amount));
+      totalIncomeAmt += amount;
     }
     else {
-        alert('Fill out entirely');
+      expenseDiv.appendChild(makeDivs(description, amount));
+      totalExpenseAmt += amount;
     }
+    totalIncome.textContent = totalIncomeAmt;
+    totalExpense.textContent = totalExpenseAmt;
+    expensePercent.textContent = `${drawPercent(totalIncomeAmt, totalExpenseAmt).toFixed(2)}%`;
+    total.textContent = drawTotal(totalIncomeAmt, totalExpenseAmt);
+    description.value = '';
+    amount.value = '';
+  }
+  else {
+    alert('Fill out entirely');
+  }
 }
 
 addForm.addEventListener('submit', handleSubmit);
 
+function divX() {
+  let
+}
+
 function makeDivs(description, amount) {
-    let newItem = document.createElement('div');
-    let itemDesc = document.createElement('p');
-    let itemAmt = document.createElement('p');
-    itemDesc.textContent = description;
-    itemAmt.textContent = amount;
-    newItem.appendChild(itemDesc);
-    newItem.appendChild(itemAmt);
-    newItem.classList.add('added');
-    return newItem;
+  let newItem = document.createElement('div');
+  let itemDesc = document.createElement('p');
+  let itemAmt = document.createElement('p');
+  itemDesc.textContent = description;
+  itemAmt.textContent = amount;
+  newItem.appendChild(itemDesc);
+  newItem.appendChild(itemAmt);
+  newItem.classList.add('added');
+  return newItem;
 }
 
 function drawTotal(income, expense) {
-    let totalAmt = income - expense;
-    if (totalAmt > 0) {
-        return `+ $${totalAmt.toFixed(2)}`;
-    }
-    else {
-        return `- $${Math.abs(totalAmt).toFixed(2)}`;
-    }
+  let totalAmt = income - expense;
+  if (totalAmt > 0) {
+    return `+ $${totalAmt.toFixed(2)}`;
+  }
+  else {
+    return `- $${Math.abs(totalAmt).toFixed(2)}`;
+  }
 }
 
 function drawPercent(income, expense) {
-    let percent = (expense / income) * 100;
-    return percent;
+  let percent = (expense / income) * 100;
+  return percent;
 }
 
 const tipCalc = document.querySelector('[data-tip-form]');
 const calcSection = document.querySelector('[data-calc]');
 
 function tipSubmit(event) {
-    event.preventDefault();
-    if (calcSection.hasChildNodes()) {
-        calcSection.removeChild(calcSection.firstChild)
-    }
-    let billAmount = parseFloat(event.target.elements.billAmount.value);
-    let quality = event.target.elements.selector.value;
-    let tipPercent;
+  event.preventDefault();
+  if (calcSection.hasChildNodes()) {
+    calcSection.removeChild(calcSection.firstChild)
+  }
+  let billAmount = parseFloat(event.target.elements.billAmount.value);
+  let quality = event.target.elements.selector.value;
+  let tipPercent;
 
-    if (quality === "Amazing") {
-        tipPercent = 0.35;
-    }
-    else if (quality === "Great") {
-        tipPercent = 0.25;
-    }
-    else if (quality === "Good") {
-        tipPercent = 0.20;
-    }
-    else if (quality === "Fine") {
-        tipPercent = 0.15;
-    }
-    else if (quality === "Eh") {
-        tipPercent = 0.10;
-    }
+  if (quality === "Amazing") {
+    tipPercent = 0.35;
+  }
+  else if (quality === "Great") {
+    tipPercent = 0.25;
+  }
+  else if (quality === "Good") {
+    tipPercent = 0.20;
+  }
+  else if (quality === "Fine") {
+    tipPercent = 0.15;
+  }
+  else if (quality === "Eh") {
+    tipPercent = 0.10;
+  }
 
-    let tipAmount = billAmount * tipPercent;
-    let total = billAmount + tipAmount;
+  let tipAmount = billAmount * tipPercent;
+  let total = billAmount + tipAmount;
 
-    let tipBox = document.createElement('p');
-    let totalBox = document.createElement('p');
+  let tipBox = document.createElement('p');
+  let totalBox = document.createElement('p');
 
-    let final = document.createElement('div');
+  let final = document.createElement('div');
 
-    tipBox.textContent = `Tip Amount: $${tipAmount.toFixed(2)}`;
-    totalBox.textContent = `Total: $${total.toFixed(2)}`;
-    final.appendChild(tipBox);
-    final.appendChild(totalBox);
-    calcSection.appendChild(final);
+  tipBox.textContent = `Tip Amount: $${tipAmount.toFixed(2)}`;
+  totalBox.textContent = `Total: $${total.toFixed(2)}`;
+  final.appendChild(tipBox);
+  final.appendChild(totalBox);
+  calcSection.appendChild(final);
 }
 
 tipCalc.addEventListener('submit', tipSubmit);
