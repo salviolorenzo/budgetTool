b4f974c0a02b96a63e40546945e43b7ba8fe9b95
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-11-28 17:16:50 -0500

    adds files and initial schema

---
+++ schema.sql
@@ -0,0 +1,24 @@
+create table users(
+    id serial primary key,
+    name varchar(50) not null,
+    email varchar(100) unique not null,
+    password varchar(100) not null,
+);
+
+create table accounts(
+    id serial primary key,
+    amount float,
+    user_id integer references users (id)
+);
+
+create table incomes(
+    id serial primary key,
+    amount float,
+    account_id integer references accounts (id)
+);
+
+create table expenses(
+    id serial primary key,
+    amount float,
+    account_id integer references accounts (id)
+);
